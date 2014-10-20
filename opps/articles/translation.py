9b554a0a6dc7d64a5ec2a577f8a0a8d7a44a9788
Paulo R <proberto.macedo@gmail.com>
2014-10-20 13:56:52 -0200

    Fixes translation

--- opps/articles/translation.py
+++ opps/articles/translation.py
@@ -4,7 +4,7 @@ from .models import Article, Post
 
 
 class ArticleTranslationOptions(TranslationOptions):
-    fields = ('title', 'hat', 'short_title', 'headline', )
+    fields = ('short_title', 'headline', )
 
 
 translator.register(Article, ArticleTranslationOptions)
